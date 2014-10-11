f790d5d55a42d581a6789cafc208ffd570522658
azureru <eru.saputra@gmail.com>
2014-10-11 09:49:17 +0700

    Adding redis recipe

---
+++ recipes/redis.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: appsindo
+# Recipe:: redis
+#
+# Copyright 2013, PT Appsindo Technology
+# @author Erwin Saputra <erwin.saputra@at.co.id>
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "redisio"
